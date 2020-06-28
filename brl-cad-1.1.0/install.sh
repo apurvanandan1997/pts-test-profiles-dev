2426d18ee21cbd95bab2898f753f7d64d7f50899
Apurva Nandan <apurva.nandan4@gmail.com>
2020-06-28 17:03:23 +0530

    Removed pnglibconf.h not found build error in brl-cad--1.1.0

--- brl-cad-1.1.0/install.sh
+++ brl-cad-1.1.0/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tar -xf brlcad-7.30.8.tar.bz2
-
+cp brlcad-7.30.8/src/other/libpng/scripts/pnglibconf.h.prebuilt brlcad-7.30.8/src/other/libpng/pnglibconf.h
 mkdir brlcad-7.30.8/build
 cd brlcad-7.30.8/build
 cmake .. -DBRLCAD_ENABLE_STRICT=NO -DBRLCAD_BUNDLED_LIBS=ON -DBRLCAD_OPTIMIZED_BUILD=ON -DCMAKE_BUILD_TYPE=Release
